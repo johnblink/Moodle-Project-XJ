20433e7e2d9886eeecd059338c4284b1a64d60d3
johnB <johndu12@hotmail.fr>
2012-03-06 23:09:49 +0100

    nhhhhhhhh

---
+++ mysimplexml/src/main/java/dcll/vheu/mysimplexml/App.java
@@ -0,0 +1,13 @@
+package dcll.vheu.mysimplexml;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World john!" );
+    }
+}
