8febd3a68ce1be6aed70ef1035f7949b94926b9a
johnB <johndu12@hotmail.fr>
2012-03-07 20:44:32 +0100

    20h44

--- mysimplexml/src/main/java/dcll/vbjj/mysimplexml/App.java
+++ mysimplexml/src/main/java/dcll/vbjj/mysimplexml/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World john  il est 20h20!" );
+        System.out.println( "Hello World john  il est 20h43!" );
     }
 }
