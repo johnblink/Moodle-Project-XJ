a456c14aa5ba893c47ae73e12c45e3bd3e0eabde
Pri4m <Jer@130.120.240.49>
2012-03-12 22:30:54 +0100

    Ajout de la class SubQuestion Mise à jours de toutes les classes pour prendre en compte les éléments Answer et SubQuestion Ajout du fichier Test.xml

--- mysimplexml/src/main/java/dcll/vbjj/mysimplexml/NumericalAnswer.java
+++ mysimplexml/src/main/java/dcll/vbjj/mysimplexml/NumericalAnswer.java
@@ -1,5 +1,7 @@
 package dcll.vbjj.mysimplexml;
 
+import java.util.ArrayList;
+
 public class NumericalAnswer extends Question{
-//
+	private ArrayList<Answer> listAnswer = new ArrayList<Answer>();
 }
--- mysimplexml/src/main/java/dcll/vbjj/mysimplexml/TrueFalse.java
+++ mysimplexml/src/main/java/dcll/vbjj/mysimplexml/TrueFalse.java
@@ -1,5 +1,7 @@
 package dcll.vbjj.mysimplexml;
 
+import java.util.ArrayList;
+
 public class TrueFalse extends Question {
-//
+	private ArrayList<Answer> listAnswer = new ArrayList<Answer>();
 }
