67380d88cddcdc9a3da3343b42cb70345113b5ec
Ben-J <bjm.brion@gmail.com>
2012-03-20 09:53:03 +0100

    MODIF UNPARSE checkstyle

--- mysimplexml/src/main/java/dcll/vbjj/mysimplexml/UnParse.java
+++ mysimplexml/src/main/java/dcll/vbjj/mysimplexml/UnParse.java
@@ -1,6 +1,5 @@
 package dcll.vbjj.mysimplexml;
 
-
 import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -21,72 +20,81 @@ public class UnParse {
 	private Element question;
 	// Iterator Question Number
 	private int nbQuestion;
-	
+
 	// Constructor
 	public UnParse() {
 		this.nbQuestion = 1;
 	}
+
 	// Save file
 	static void enregistre(String fichier) {
-	   try {
-	      //Classic format with getPrettyFormat()
-	      XMLOutputter sortie = new XMLOutputter(Format.getPrettyFormat());
-	      //Serialization.
-	      sortie.output(document, new FileOutputStream(fichier));
-	   } catch (java.io.IOException e){}
-	}	
-	// Print File
-	static void affiche()
-	{
-	   try {
-	      // Classic Format getPrettyFormat()
-	      XMLOutputter sortie = new XMLOutputter(Format.getPrettyFormat());
-	      sortie.output(document, System.out);
-	   } catch (java.io.IOException e){}
+		try {
+			// Classic format with getPrettyFormat()
+			XMLOutputter sortie = new XMLOutputter(Format.getPrettyFormat());
+			// Serialization.
+			sortie.output(document, new FileOutputStream(fichier));
+		} catch (java.io.IOException e) {
+		}
 	}
+
+	// Print File
+	static void affiche() {
+		try {
+			// Classic Format getPrettyFormat()
+			XMLOutputter sortie = new XMLOutputter(Format.getPrettyFormat());
+			sortie.output(document, System.out);
+		} catch (java.io.IOException e) {
+		}
+	}
+
 	// Function use for add simple tag (NOM with attribute) with text tag (VAR)
-	private void ajouteBaliseSimpleAvecTexte(String nom, String attribute, String var) {
+	private void ajouteBaliseSimpleAvecTexte(String nom, String attribute,
+			String var) {
 		Element balise = new Element(nom);
 		Element text = new Element("text");
-		if(!attribute.equals("")){
-			Attribute classe = new Attribute("format",attribute);
+		if (!attribute.equals("")) {
+			Attribute classe = new Attribute("format", attribute);
 			balise.setAttribute(classe);
 		}
 		text.setText(var);
 		balise.addContent(text);
 		this.question.addContent(balise);
 	}
+
 	// Function use for IMAGE tag
 	private void ajouteBaliseImage(String nom, String source) {
 		Element image = new Element(nom);
-		if(!source.equals("")) {
+		if (!source.equals("")) {
 			image.setText(source);
 		}
 		this.question.addContent(image);
 	}
+
 	// Function use for FEEDBACK tag
 	private void ajouteBaliseGeneralFeedback(String var) {
 		Element generalfeedback = new Element("generalfeedback");
 		Element text = new Element("text");
-		
+
 		text.setText(var);
 		generalfeedback.addContent(text);
 		this.question.addContent(generalfeedback);
 	}
+
 	// Function use for DEFAULTGRADE, PENALTY, HIDDEN, SHUFFLEANSWERS tags
 	private void ajouteBaliseSimple(String nom, String var) {
 		Element balise = new Element(nom);
-		
+
 		balise.setText(var);
 		this.question.addContent(balise);
-	}	
-	// Add Tag Answer  
+	}
+
+	// Add Tag Answer
 	private void ajouteBaliseAnswer(Answer a) {
 		Element answer = new Element("answer");
 		Element text = new Element("text");
 		Element textfb = new Element("text");
-		Element feedback =  new Element("feedback");
-		Attribute classe = new Attribute("fraction",a.getFraction());
+		Element feedback = new Element("feedback");
+		Attribute classe = new Attribute("fraction", a.getFraction());
 		answer.setAttribute(classe);
 		text.setText(a.getText());
 		answer.addContent(text);
@@ -95,19 +103,21 @@ public class UnParse {
 		answer.addContent(feedback);
 		this.question.addContent(answer);
 	}
-	// Add Tags in Answers List 
+
+	// Add Tags in Answers List
 	private void ajouteListeBalisesAnswer(ArrayList<Answer> l) {
 		Iterator<Answer> i = l.iterator();
-		
-		while(i.hasNext())
-			ajouteBaliseAnswer((Answer)i.next());
+
+		while (i.hasNext())
+			ajouteBaliseAnswer((Answer) i.next());
 	}
+
 	// Add Tags in SubQuestions List
-	private void ajouteBaliseSubQuestion( ArrayList<SubQuestion> l) {
+	private void ajouteBaliseSubQuestion(ArrayList<SubQuestion> l) {
 		Element subquestion, text, answer;
-		
+
 		Iterator<SubQuestion> i = l.iterator();
-		while(i.hasNext()) {
+		while (i.hasNext()) {
 			subquestion = new Element("subquestion");
 			text = new Element("text");
 			text.setText(i.next().getText());
@@ -120,149 +130,234 @@ public class UnParse {
 			this.question.addContent(subquestion);
 		}
 	}
-	
-	
+
 	// Add Tag Question Calculated
 	private void ajouteBaliseQuestionCalculated(Calculated d) {
 		System.out.println("Question calculated");/* TEST LIGN -> */
-		Attribute classe = new Attribute("type","calculated");
+		Attribute classe = new Attribute("type", "calculated");
 		this.question.setAttribute(classe);
-		
-		ajouteBaliseSimpleAvecTexte("name","",d.getName());/* ADD TAG NAME */
-		ajouteBaliseSimpleAvecTexte("questiontext", "moodle_auto_format", d.getQuestionText());/* ADD TAG QUESTION TEXT */
-		ajouteBaliseImage("image", "");/* ADD TAG IMAGE */
-		ajouteBaliseSimpleAvecTexte("generalfeedback", "", d.getGeneralFeedback());/* ADD TAG GENERALFEEDBACK */
-		ajouteBaliseSimple("defaultGrade", d.getDefaultGrade());/* ADD TAG DEFAULTGRADE */
-		ajouteBaliseSimple("penalty", d.getPenalty());/* ADD TAG PENALTY */
-		ajouteBaliseSimple("hidden", d.getHidden());/* ADD TAG HIDDEN */
-		ajouteBaliseAnswer(d.getAnswer());/* ADD TAG SHUFFLEANSWERS */
-		
+
+		// ADD TAG NAME
+		ajouteBaliseSimpleAvecTexte("name", "", d.getName());
+		// ADD TAG QUESTION TEXT
+		ajouteBaliseSimpleAvecTexte("questiontext", "moodle_auto_format",
+				d.getQuestionText());
+		// ADD TAG IMAGE
+		ajouteBaliseImage("image", "");
+		// ADD TAG GENERALFEEDBACK
+		ajouteBaliseSimpleAvecTexte("generalfeedback", "",
+				d.getGeneralFeedback());
+		// ADD TAG DEFAULTGRADE
+		ajouteBaliseSimple("defaultGrade", d.getDefaultGrade());
+		// ADD TAG PENALTY
+		ajouteBaliseSimple("penalty", d.getPenalty());
+		// ADD TAG HIDDEN
+		ajouteBaliseSimple("hidden", d.getHidden());
+		// ADD TAG SHUFFLEANSWERS
+		ajouteBaliseAnswer(d.getAnswer());
 		// Put on trunk
 		racine.addContent(this.question);
 	}
+
 	// Add Tag Question Essay
 	private void ajouteBaliseQuestionEssay(Essay e) {
 		System.out.println("Question essay");/* TEST LIGN -> */
-		Attribute classe = new Attribute("type","essay");
+		Attribute classe = new Attribute("type", "essay");
 		this.question.setAttribute(classe);
-		
-		ajouteBaliseSimpleAvecTexte("name", "", e.getName());/* ADD TAG NAME */
-		ajouteBaliseSimpleAvecTexte("questiontext", "moodle_auto_format", e.getQuestionText());/* ADD TAG QUESTION TEXT */
-		ajouteBaliseImage("image", "");/* ADD TAG IMAGE */
-		ajouteBaliseGeneralFeedback(e.getGeneralFeedback());/* ADD TAG GENERALFEEDBACK */
-		ajouteBaliseSimple("defaultGrade",e.getDefaultGrade());/* ADD TAG DEFAULTGRADE */
-		ajouteBaliseSimple("penalty",e.getPenalty());/* ADD TAG PENALTY */
-		ajouteBaliseSimple("hidden",e.getHidden());/* ADD TAG HIDDEN */
-		ajouteBaliseSimple("shuffleanswers",e.getShuffleAnswers());/* ADD TAG SHUFFLEANSWERS */
-		ajouteBaliseAnswer(e.getAnswer());/* ADD TAG ANSWER */
+
+		// ADD TAG NAME
+		ajouteBaliseSimpleAvecTexte("name", "", e.getName());
+		// ADD TAG QUESTION TEXT
+		ajouteBaliseSimpleAvecTexte("questiontext", "moodle_auto_format",
+				e.getQuestionText());
+		// ADD TAG IMAGE
+		ajouteBaliseImage("image", "");
+		// ADD TAG GENERALFEEDBACK
+		ajouteBaliseGeneralFeedback(e.getGeneralFeedback());
+		// ADD TAG DEFAULTGRADE
+		ajouteBaliseSimple("defaultGrade", e.getDefaultGrade());
+		// ADD TAG PENALTY
+		ajouteBaliseSimple("penalty", e.getPenalty());
+		// ADD TAG HIDDEN
+		ajouteBaliseSimple("hidden", e.getHidden());
+		// ADD TAG SHUFFLEANSWERS
+		ajouteBaliseSimple("shuffleanswers", e.getShuffleAnswers());
+		// ADD TAG ANSWER
+		ajouteBaliseAnswer(e.getAnswer());
 		// Put on trunk
 		racine.addContent(this.question);
 	}
+
 	// Add Tag Question Numerical
-	private void ajouteBaliseQuestionNumerical (NumericalAnswer a) {
+	private void ajouteBaliseQuestionNumerical(NumericalAnswer a) {
 		System.out.println("Question numericalanswer");/* TEST LIGN -> */
-		Attribute classe = new Attribute("type","numerical");
+		Attribute classe = new Attribute("type", "numerical");
 		question.setAttribute(classe);
-		
-		ajouteBaliseSimpleAvecTexte("name","",a.getName());/* ADD TAG NAME */
-		ajouteBaliseSimpleAvecTexte("questiontext", "moodle_auto_format", a.getQuestionText());/* ADD TAG QUESTION TEXT */
-		ajouteBaliseImage("image", "");/* ADD TAG IMAGE */
-		ajouteBaliseGeneralFeedback(a.getGeneralFeedback());/* ADD TAG GENERALFEEDBACK */
-		ajouteBaliseSimple("defaultGrade", a.getDefaultGrade());/* ADD TAG DEFAULTGRADE */
-		ajouteBaliseSimple("penalty", a.getPenalty());/* ADD TAG PENALTY */
-		ajouteBaliseSimple("hidden", a.getHidden());/* ADD TAG HIDDEN */
-		ajouteBaliseSimple("shuffleanswers", a.getShuffleAnswers());/* ADD TAG SHUFFLEANSWERS */
-		ajouteBaliseAnswer(a.getAnswer());/* ADD TAG ANSWER */
+
+		// ADD TAG NAME
+		ajouteBaliseSimpleAvecTexte("name", "", a.getName());
+		// ADD TAG QUESTION TEXT
+		ajouteBaliseSimpleAvecTexte("questiontext", "moodle_auto_format",
+				a.getQuestionText());
+		// ADD TAG IMAGE
+		ajouteBaliseImage("image", "");
+		// ADD TAG GENERALFEEDBACK
+		ajouteBaliseGeneralFeedback(a.getGeneralFeedback());
+		// ADD TAG DEFAULTGRADE
+		ajouteBaliseSimple("defaultGrade", a.getDefaultGrade());
+		// ADD TAG PENALTY
+		ajouteBaliseSimple("penalty", a.getPenalty());
+		// ADD TAG HIDDEN
+		ajouteBaliseSimple("hidden", a.getHidden());
+		// ADD TAG SHUFFLEANSWERS
+		ajouteBaliseSimple("shuffleanswers", a.getShuffleAnswers());
+		// ADD TAG ANSWER
+		ajouteBaliseAnswer(a.getAnswer());
 		// Put on trunk
 		racine.addContent(this.question);
 	}
+
 	// Add Tag Question Matching
-	private void ajouteBaliseQuestionMatching (Matching m) {
+	private void ajouteBaliseQuestionMatching(Matching m) {
 		System.out.println("Question matching");/* TEST LIGN -> */
-		Attribute classe = new Attribute("type","matching");
+		Attribute classe = new Attribute("type", "matching");
 		question.setAttribute(classe);
-		
-		ajouteBaliseSimpleAvecTexte("name","",m.getName());/* ADD TAG NAME */
-		ajouteBaliseSimpleAvecTexte("questiontext", "moodle_auto_format", m.getQuestionText());/* ADD TAG QUESTION TEXT */
-		ajouteBaliseImage("image", "");/* ADD TAG IMAGE */
-		ajouteBaliseSimpleAvecTexte("generalfeedback", "", m.getGeneralFeedback());/* ADD TAG GENERALFEEDBACK */
-		ajouteBaliseSimple("defaultGrade", m.getDefaultGrade());/* ADD TAG DEFAULTGRADE */
-		ajouteBaliseSimple("penalty", m.getPenalty());/* ADD TAG PENALTY */
-		ajouteBaliseSimple("hidden", m.getHidden());/* ADD TAG HIDDEN */
-		ajouteBaliseSimple("shuffleanswers", m.getShuffleAnswers());/* ADD TAG SHUFFLEANSWERS */
-		ajouteBaliseSubQuestion(m.getListSubQuestion());/* ADD TAG SUBQUESTIONS */
-		
+
+		// ADD TAG NAME
+		ajouteBaliseSimpleAvecTexte("name", "", m.getName());
+		// ADD TAG QUESTION TEXT
+		ajouteBaliseSimpleAvecTexte("questiontext", "moodle_auto_format",
+				m.getQuestionText());
+		// ADD TAG IMAGE
+		ajouteBaliseImage("image", "");
+		// ADD TAG GENERALFEEDBACK
+		ajouteBaliseSimpleAvecTexte("generalfeedback", "",
+				m.getGeneralFeedback());
+		// ADD TAG DEFAULTGRADE
+		ajouteBaliseSimple("defaultGrade", m.getDefaultGrade());
+		// ADD TAG PENALTY
+		ajouteBaliseSimple("penalty", m.getPenalty());
+		// ADD TAG HIDDEN
+		ajouteBaliseSimple("hidden", m.getHidden());
+		// ADD TAG SHUFFLEANSWERS
+		ajouteBaliseSimple("shuffleanswers", m.getShuffleAnswers());
+		// ADD TAG SUBQUESTIONS
+		ajouteBaliseSubQuestion(m.getListSubQuestion());
 		// Put on trunk
 		racine.addContent(this.question);
 	}
+
 	// Add Tag Question Multiple Choice
-	private void ajouteBaliseQuestionMultipleChoice( MultipleChoice mc){
+	private void ajouteBaliseQuestionMultipleChoice(MultipleChoice mc) {
 		System.out.println("Question multichoice");/* TEST LIGN -> */
-		Attribute classe = new Attribute("type","multichoice");
+		Attribute classe = new Attribute("type", "multichoice");
 		question.setAttribute(classe);
-		
-		ajouteBaliseSimpleAvecTexte("name", "", mc.getName());/* ADD TAG NAME */
-		ajouteBaliseSimpleAvecTexte("questiontext", "html", mc.getQuestionText());/* ADD TAG QUESTION TEXT */
-		ajouteBaliseImage("image", mc.getImage());/* ADD TAG IMAGE */
-		ajouteBaliseImage("image_base64", mc.getImage_base64());/* ADD TAG IMAGE */
-		ajouteBaliseSimpleAvecTexte("generalfeedback","",mc.getGeneralFeedback());/* ADD TAG GENERALFEEDBACK */
-		ajouteBaliseSimple("defaultGrade", mc.getDefaultGrade());/* ADD TAG DEFAULTGRADE */
-		ajouteBaliseSimple("penalty", mc.getPenalty());/* ADD TAG PENALTY */
-		ajouteBaliseSimple("hidden", mc.getHidden());/* ADD TAG HIDDEN */
-		ajouteBaliseSimple("shuffleanswers", mc.getShuffleAnswers1());/* ADD TAG SHUFFLEANSWERS */
-		ajouteBaliseSimple("single", mc.getSingle());/* ADD TAG SINGLE */
-		ajouteBaliseSimple("shuffleanswers2", mc.getShuffleAnswers1());/* ADD TAG SHUFFLEANSWERS */
-		ajouteBaliseSimpleAvecTexte("correctfeedback","",mc.getCorrectFeedback());/* ADD TAG CORRECTFEEDBACK */
-		ajouteBaliseSimpleAvecTexte("partiallycorrectfeedback","",mc.getPartiallyCorrectFeedback());/* ADD TAG PARTIALLYCORRECTFEEDBACK */
-		ajouteBaliseSimpleAvecTexte("incorrectfeedback","",mc.getIncorrectFeedback());/* ADD TAG INCORRECTFEEDBACK */
-		ajouteBaliseSimple("answernumbering", mc.getAnswerNumbering());/* ADD TAG SINGLE */
-		ajouteListeBalisesAnswer(mc.getListAnswer());/* ADD TAGS ANSWER */
+
+		// ADD TAG NAME
+		ajouteBaliseSimpleAvecTexte("name", "", mc.getName());
+		// ADD TAG QUESTION TEXT
+		ajouteBaliseSimpleAvecTexte("questiontext", "html",
+				mc.getQuestionText());
+		// ADD TAG IMAGE
+		ajouteBaliseImage("image", mc.getImage());
+		// ADD TAG IMAGE
+		ajouteBaliseImage("image_base64", mc.getImage_base64());
+		// ADD TAG GENERALFEEDBACK
+		ajouteBaliseSimpleAvecTexte("generalfeedback", "",
+				mc.getGeneralFeedback());
+		// ADD TAG DEFAULTGRADE
+		ajouteBaliseSimple("defaultGrade", mc.getDefaultGrade());
+		// ADD TAG PENALTY
+		ajouteBaliseSimple("penalty", mc.getPenalty());
+		// ADD TAG HIDDEN
+		ajouteBaliseSimple("hidden", mc.getHidden());
+		// ADD TAG SHUFFLEANSWERS
+		ajouteBaliseSimple("shuffleanswers", mc.getShuffleAnswers1());
+		// ADD TAG SINGLE
+		ajouteBaliseSimple("single", mc.getSingle());
+		// ADD TAG SHUFFLEANSWERS
+		ajouteBaliseSimple("shuffleanswers2", mc.getShuffleAnswers1());
+		// ADD TAG CORRECTFEEDBACK
+		ajouteBaliseSimpleAvecTexte("correctfeedback", "",
+				mc.getCorrectFeedback());
+		// ADD TAG PARTIALLYCORRECTFEEDBACK
+		ajouteBaliseSimpleAvecTexte("partiallycorrectfeedback", "",
+				mc.getPartiallyCorrectFeedback());
+		// ADD TAG INCORRECTFEEDBACK
+		ajouteBaliseSimpleAvecTexte("incorrectfeedback", "",
+				mc.getIncorrectFeedback());
+		// ADD TAG SINGLE
+		ajouteBaliseSimple("answernumbering", mc.getAnswerNumbering());
+		// ADD TAGS ANSWER
+		ajouteListeBalisesAnswer(mc.getListAnswer());
 		// Put on trunk
 		racine.addContent(this.question);
 	}
+
 	// Add Tag Question Short Answer
 	private void ajouteBaliseQuestionShortAnswer(ShortAnswer sa) {
-
-		System.out.println("Question shortanswer");/* TEST LIGN -> */
-		Attribute classe = new Attribute("type","shortanswer");
+		// TEST LIGN ->
+		System.out.println("Question shortanswer");
+		Attribute classe = new Attribute("type", "shortanswer");
 		question.setAttribute(classe);
-		
-		ajouteBaliseSimpleAvecTexte("name","",sa.getName());/* ADD TAG NAME */
-		ajouteBaliseSimpleAvecTexte("questiontext", "moodle_auto_format", sa.getQuestionText());/* ADD TAG QUESTION TEXT */
-		ajouteBaliseImage("image", "");/* ADD TAG IMAGE */
-		ajouteBaliseGeneralFeedback(sa.getGeneralFeedback());/* ADD TAG GENERALFEEDBACK */
-		ajouteBaliseSimple("defaultGrade", sa.getDefaultGrade());/* ADD TAG DEFAULTGRADE */
-		ajouteBaliseSimple("penalty", sa.getPenalty());/* ADD TAG PENALTY */
-		ajouteBaliseSimple("hidden", sa.getHidden());/* ADD TAG HIDDEN */
-		ajouteBaliseSimple("shuffleanswers", sa.getShuffleAnswers());/* ADD TAG SHUFFLEANSWERS */
-		ajouteBaliseSimple("usecase", sa.getUseCase());/* ADD TAG USECASE */
-		ajouteListeBalisesAnswer(sa.getListAnswer());/* ADD TAGS ANSWER */
+
+		// ADD TAG NAME
+		ajouteBaliseSimpleAvecTexte("name", "", sa.getName());
+		// ADD TAG QUESTION TEXT
+		ajouteBaliseSimpleAvecTexte("questiontext", "moodle_auto_format",
+				sa.getQuestionText());
+		// ADD TAG IMAGE
+		ajouteBaliseImage("image", "");
+		// ADD TAG GENERALFEEDBACK
+		ajouteBaliseGeneralFeedback(sa.getGeneralFeedback());
+		// ADD TAG DEFAULTGRADE
+		ajouteBaliseSimple("defaultGrade", sa.getDefaultGrade());
+		// ADD TAG PENALTY
+		ajouteBaliseSimple("penalty", sa.getPenalty());
+		// ADD TAG HIDDEN
+		ajouteBaliseSimple("hidden", sa.getHidden());
+		// ADD TAG SHUFFLEANSWERS
+		ajouteBaliseSimple("shuffleanswers", sa.getShuffleAnswers());
+		// ADD TAG USECASE
+		ajouteBaliseSimple("usecase", sa.getUseCase());
+		// ADD TAGS ANSWER
+		ajouteListeBalisesAnswer(sa.getListAnswer());
 		// Put on trunk
 		racine.addContent(this.question);
 	}
+
 	// Add Tag Question True False
 	private void ajouteBaliseQuestionTrueFalse(TrueFalse tf) {
 		System.out.println("Question truefalse");/* TEST LIGN -> */
-		Attribute classe = new Attribute("type","truefalse");
+		Attribute classe = new Attribute("type", "truefalse");
 		question.setAttribute(classe);
-		
-		ajouteBaliseSimpleAvecTexte("name", "", tf.getName());/* ADD TAG NAME */
-		ajouteBaliseSimpleAvecTexte("questiontext", "moodle_auto_format", tf.getQuestionText());/* ADD TAG QUESTION TEXT */
-		ajouteBaliseImage("image", "");/* ADD TAG IMAGE */
-		ajouteBaliseGeneralFeedback(tf.getGeneralFeedback());/* ADD TAG GENERALFEEDBACK */
-		ajouteBaliseSimple("defaultGrade", tf.getDefaultGrade());/* ADD TAG DEFAULTGRADE */
-		ajouteBaliseSimple("penalty", tf.getPenalty());/* ADD TAG PENALTY */
-		ajouteBaliseSimple("hidden", tf.getHidden());/* ADD TAG HIDDEN */
-		ajouteBaliseSimple("shuffleanswers", tf.getShuffleAnswers());/* ADD TAG SHUFFLEANSWERS */
-		ajouteListeBalisesAnswer(tf.getListAnswer());/* ADD TAGS ANSWER */
+
+		// ADD TAG NAME
+		ajouteBaliseSimpleAvecTexte("name", "", tf.getName());
+		// ADD TAG QUESTION TEXT
+		ajouteBaliseSimpleAvecTexte("questiontext", "moodle_auto_format",
+				tf.getQuestionText());
+		// ADD TAG IMAGE
+		ajouteBaliseImage("image", "");
+		// ADD TAG GENERALFEEDBACK
+		ajouteBaliseGeneralFeedback(tf.getGeneralFeedback());
+		// ADD TAG DEFAULTGRADE
+		ajouteBaliseSimple("defaultGrade", tf.getDefaultGrade());
+		// ADD TAG PENALTY
+		ajouteBaliseSimple("penalty", tf.getPenalty());
+		// ADD TAG HIDDEN
+		ajouteBaliseSimple("hidden", tf.getHidden());
+		// ADD TAG SHUFFLEANSWERS
+		ajouteBaliseSimple("shuffleanswers", tf.getShuffleAnswers());
+		// ADD TAGS ANSWER
+		ajouteListeBalisesAnswer(tf.getListAnswer());
 		// Put on trunk
 		racine.addContent(this.question);
 	}
-	
-	
+
 	// Write A Question
 	private void parcoursQuestions(Question q) {
-		switch((String)q.getClass().getName()){
+		switch ((String) q.getClass().getName()) {
 		case "dcll.vbjj.mysimplexml.Calculated":
 			ajouteBaliseQuestionCalculated((Calculated) q);
 			break;
@@ -287,19 +382,19 @@ public class UnParse {
 		default:
 		}
 	}
-	
-	public void run (ArrayList<Question> lq){
+
+	public void run(ArrayList<Question> lq) {
 		Comment comment;
-		
+
 		Iterator<Question> i = lq.iterator();
-		while(i.hasNext()) {
+		while (i.hasNext()) {
 			// Every iteration create a new tag question
 			this.question = new Element("question");
 			// Add number comment for each question
-			comment = new Comment("question: "+nbQuestion++);
+			comment = new Comment("question: " + nbQuestion++);
 			racine.addContent(comment);
-			
-			parcoursQuestions((Question)i.next());
+
+			parcoursQuestions((Question) i.next());
 		}
 		enregistre("src/main/java/xmlsample/quiz.xml");
 	}
