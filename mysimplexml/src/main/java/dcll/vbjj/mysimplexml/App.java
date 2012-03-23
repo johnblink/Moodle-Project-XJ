308d52c15dfe4c06284abf171a872c4f584a41a9
Pri4m <Jer@130.120.240.49>
2012-03-23 15:47:14 +0100

    Commentaire APP

--- mysimplexml/src/main/java/dcll/vbjj/mysimplexml/App.java
+++ mysimplexml/src/main/java/dcll/vbjj/mysimplexml/App.java
@@ -3,13 +3,15 @@ package dcll.vbjj.mysimplexml;
 import java.util.ArrayList;
 
 public class App {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
+    	//Creation du parser
         Parse test = new Parse();
         test.xml2Java("src/main/java/xmlsample/Test.xml");
         test.affiche();
         ArrayList <Question> l = test.getQuestions();
         System.out.println(l.size());
+        
+        //Creation de l'objet permettant l'enregistrement des questions dans le xml
         UnParse unparse = new UnParse();
 		unparse.run(l);
     }
